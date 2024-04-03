f41d93b5f71ea453ed8a485ad2457a502f5f8684
Aditya137x <111834313+Aditya137x@users.noreply.github.com>
2024-04-03 12:44:34 +0530

    Add multiple illustions and a way to navigate between them

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,47 +3,38 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
@@ -66,13 +57,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArtImage(modifier: Modifier = Modifier) {
+fun ArtImage() {
     Column (modifier = Modifier
         .fillMaxSize()
         .padding(40.dp)
         .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween) {
+        var result by remember { mutableIntStateOf( 1) }
+        val imageResource = when(result) {
+            1 -> R.drawable.sunday
+            2 -> R.drawable.aday
+            3 -> R.drawable.market
+            4 -> R.drawable.book
+            5 -> R.drawable.cafe
+            else -> R.drawable.draw
+        }
+
+        val textResource = when(result) {
+            1 -> R.string.sunday
+            else -> R.string.day
+        }
+
         ElevatedCard(
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 6.dp
@@ -82,7 +88,7 @@ fun ArtImage(modifier: Modifier = Modifier) {
                 .align(Alignment.CenterHorizontally)
         ) {
             Image(
-                painter = painterResource(R.drawable.sunday),
+                painter = painterResource(imageResource),
                 contentDescription = null,
                 modifier = Modifier
                     .border(10.dp, MaterialTheme.colorScheme.primaryContainer)
@@ -90,6 +96,8 @@ fun ArtImage(modifier: Modifier = Modifier) {
                     .shadow(10.dp),
             )
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         ElevatedCard(
             elevation = CardDefaults.cardElevation(
@@ -101,7 +109,7 @@ fun ArtImage(modifier: Modifier = Modifier) {
 
         ) {
             Text(
-                text = "A Sunday Illsutration",
+                text = stringResource(textResource),
                 fontSize = 20.sp,
                 textAlign = TextAlign.Center,
                 modifier= Modifier.padding(15.dp,15.dp,10.dp,5.dp)
@@ -114,30 +122,62 @@ fun ArtImage(modifier: Modifier = Modifier) {
             )
         }
 
+        Spacer(modifier = Modifier.height(20.dp))
 
+        if (result==1) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = { result++ }) {
+                    Text(text = "Next", modifier = Modifier.padding(horizontal = 13.dp))
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 15.dp),
-            verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Button(onClick = { /*TODO*/ }, ) {
-                Text(text = "Previous")
-
+                }
             }
-            //Spacer(modifier = Modifier.fillMaxWidth(0.4F))
+        }
+        else if (result==6) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = { result-- }) {
+                    Text(text = "Previous")
 
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Next", modifier = Modifier.padding(horizontal = 13.dp))
+                }
+            }
+        }
+        else {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(onClick = { result-- }) {
+                    Text(text = "Previous")
 
+                }
+
+                Button(onClick = { result++ }) {
+                    Text(text = "Next", modifier = Modifier.padding(horizontal = 13.dp))
+
+                }
             }
         }
 
 
+
     }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
